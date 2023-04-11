51ff63864ff3357a906a6b8cab985d980770649f
aridwan1037 <128478985+aridwan1037@users.noreply.github.com>
2023-04-11 12:16:24 +0700

    Dispose And GC

---
+++ Day12/dISPOSE/Program.cs
@@ -0,0 +1,45 @@
+﻿class Resource : IDisposable  
+    {  
+        private bool disposed = false;  
+        public void Dispose()  
+        {  
+            Dispose(true);  
+            GC.SuppressFinalize(this);  
+        }  
+  
+        private void Dispose(bool disposing)  
+        {  
+            if (!disposed)  
+            {  
+                if (disposing)  
+                {  
+                    // can clean up other managed objects  
+                }  
+                // clean up unmanaged resources  
+                disposed = true;  
+            }  
+        }  
+        ~Resource()  
+        {  
+            Dispose(false);  
+        }  
+    }  
+    class Program  
+    {  
+        static void Main()  
+        {  
+            Resource resource = null;  
+            try  
+            {  
+                resource = new Resource();  
+                // use resource  
+            }  
+            finally  
+            {  
+                if (resource != null)  
+                {  
+                    resource.Dispose();  
+                }  
+            }  
+        }  
+    }  
