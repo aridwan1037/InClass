bf94d143e2ccadd374a8f73c6428d0e9a9ba1948
aridwan1037 <128478985+aridwan1037@users.noreply.github.com>
2023-05-11 00:23:56 +0700

    debug technique

---
+++ Day32/GetAFreshView/Calculator.cs
@@ -0,0 +1,17 @@
+namespace GetAFreshView;
+
+public class Calculator
+{
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
+    public int Sub(int x, int y)
+    {
+        return x + y;
+    }
+    public int Mul(int x, int y)
+    {
+        return x * y;
+    }
+}
---
+++ Day32/GetAFreshView/Program.cs
@@ -0,0 +1,18 @@
+﻿using GetAFreshView;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Calculator cal = new Calculator();
+        int a = 5;
+        int b = 3;
+        int add = cal.Add(a, b);
+        int sub = cal.Sub(a, b);
+        int mul = cal.Mul(a, b);
+
+        Console.WriteLine($" Addition Method Result = {add} ");
+        Console.WriteLine($" Subtraction Method Result = {sub}");
+        Console.WriteLine($" Multiplication Method Result = {mul}");
+        
+    }
+}
